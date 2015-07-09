92b7241302b3682bf78bcf1e64609de106f8d386
vatseek <vadim.gumennyj@gmail.com>
2015-07-09 21:43:29 +0300

    add Exception

--- src/Migrator/Client.php
+++ src/Migrator/Client.php
@@ -2,10 +2,36 @@
 
 namespace Migrator;
 
+use Migrator\Exception\MigrationException;
+
 class Client
 {
-    public static function world()
+    protected $dbHost = false;
+    protected $dbName = false;
+    protected $dbUser = false;
+    protected $dbPass = false;
+
+    protected $connection = false;
+
+    public function __construct($dbName, $dbUser, $dbPass, $dbHost = 'localhost')
     {
-        return 'Hello World, Composer!';
+        $this->dbHost = $dbHost;
+        $this->dbName = $dbName;
+        $this->dbUser = $dbUser;
+        $this->dbPass = $dbPass;
+
+        $this->connection = @mysql_connect($this->dbHost, $this->dbUser, $this->dbPass);
+        if (!$this->connection) {
+            throw new MigrationException('MySQL connect error');
+        }
+
+        if (!@mysql_select_db('information_schema')) {
+            throw new MigrationException('MySQL access error');
+        }
+    }
+
+    public function run()
+    {
+        die('123');
     }
 }
---
+++ src/Migrator/Exception/MigrationException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Migrator\Exception;
+
+class MigrationException extends \Exception
+{
+
+}
